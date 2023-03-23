7961f6d0fc39ed534914b147cafd68a66db453c1
jiangmin <7548764@qq.com>
2023-03-23 14:32:11 +0800

    feat: upload file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description: 
- * @Author: jiangmin
- * @Date: 2021-12-29 00:41:23
- * @LastEditTime: 2021-12-29 00:41:23
- * @LastEditors: jiangmin
- * @Reference: 
- */
-import "./index.scss";
